d3bd464f396e7a209783cc166cb4cadd382f6357
Brad Schlicht <brad.schlicht@chrobinson.com>
2021-09-19 15:31:27 -0500

    update names to be more clear

--- scripts/dockerweb.sh
+++ scripts/dockerweb.sh
@@ -13,23 +13,23 @@ echo 'type=83' | sudo sfdisk /dev/xvdh
 # Create file system on attached EBS volume
 mkfs -t ext4 /dev/xvdh1
 
-# Setup local webroot directory
-mkdir /webroot
-chmod 777 /webroot
+# Setup local web root directory for nginx container
+mkdir /www
+chmod 777 /www
 
 # Ensure filesystem on EBS volume is mounted automatically on reboot
 cat << EOF >> /etc/fstab
-/dev/xvdh1   /webroot          ext4 defaults             0 2
+/dev/xvdh1   /www          ext4 defaults             0 2
 EOF
 
 # Mount the entry we defined in fstab
 sudo mount -a
 
 # Add index file and link to terraform code
-cat << EOF > /webroot/index.html
+cat << EOF > /www/index.html
 <h1>Hello AWS World</h1>
-<a href="https://github.com/schlbra/aws-exercise-1">terraform source code</a>
+<a href="https://github.com/schlbra/aws-exercise-1">Exercise Source Code</a>
 EOF
 
 # Run NGINX container to host index page
-docker run -d --name awsexercise1 -v /webroot:/usr/share/nginx/html:ro -p 80:80 --restart always nginx
+docker run -d --name awsexercise1 -v /www:/usr/share/nginx/html:ro -p 80:80 --restart always nginx
